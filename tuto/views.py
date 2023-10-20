5b1a5dd713e04a8541c70448157ef38e7ffb17fb
noa menjikoff <noa.menjikoff@gmail.com>
2023-10-20 10:47:05 +0200

    tp4: debut de l'ajout du formulaire

--- tuto/views.py
+++ tuto/views.py
@@ -1,20 +1,46 @@
 from .app import app
 from flask import render_template
 from .models import get_sample
-
+from flask_wtf import FlaskForm
+from wtforms import StringField , HiddenField
+from wtforms . validators import DataRequired
+from flask import url_for , redirect
+from .app import db
+from . models import Author
 
 @app.route("/")
 def home():
-    return render_template(
-        "booksBS.html", 
-        title="My Books !",
-        books=get_sample())
+    return render_template("booksBS.html", title="My Books !", books=get_sample())
 
 
 @app.route("/detail/<id>")
 def detail(id):
     books = get_sample()
     book = books[int(id)-1]
-    return render_template(
-        "detail.html",
-        book=book)
+    return render_template("detail.html", book=book)
+
+class AuthorForm (FlaskForm):
+    id = HiddenField ("id")
+    name = StringField ("Nom", validators =[DataRequired()])
+
+
+@app.route("/edit/author/<int:id>")
+def edit_author(id):
+    a = get_author(id)
+    f = AuthorForm(id=a.id , name=a.name)
+    return render_template("edit-author.html", author=a, form=f)
+
+
+
+@app.route("/save/author/", methods =("POST",))
+def save_author ():
+    a = None
+    f = AuthorForm()
+    if f.validate_on_submit():
+        id = int(f.id.data)
+        a = get_author(id)
+        a.name = f.name.data
+        db.session.commit ()
+        return redirect(url_for("one_author", id=a.id))
+    a = get_author(int(f.id.data))
+    return render_template ("edit-author.html", author =a, form=f)
